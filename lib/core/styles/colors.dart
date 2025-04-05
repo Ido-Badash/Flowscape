5ef4d8846e19a4209528127e0334bda78359bcd0
Ido-Badash <idoba12012011@gmail.com>
2025-04-05 21:38:57 +0300

    ✨ Refactor FlowColors class to update color definitions and improve organization

--- lib/core/styles/colors.dart
+++ lib/core/styles/colors.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+
 class FlowColors {
-  static const Color blue = Color.fromRGBO(120, 130, 255, 1.0);
-  static const Color darkBlue = Color.fromRGBO(12, 12, 15, 1.0);
+  static Color body = const Color.fromRGBO(15, 23, 42, 1.0); // for screens body
+  static Color bars = const Color.fromRGBO(30, 41, 59, 1.0); // for app bars and nav bar
 
-  static const Color lightGray = Color.fromRGBO(172, 172, 198, 1.0);
+  static Color text1 = const Color.fromRGBO(248, 250, 252, 1.0); // for normal text like in the nav icon buttons
+  static Color text2 = const Color.fromRGBO(203, 213, 225, 1.0); // for nav bar icons buttons and placeholder text
+  static Color text3 = const Color.fromRGBO(148, 163, 184, 1.0); // stuff like the quote genrator and icon in home-scapes screen
+  static Color text4 = const Color.fromRGBO(100, 116, 139, 1.0); // for page title in top left
 
-  static const Color pink = Color.fromRGBO(202, 5, 77, 1.0);
-
-  static const Color lavender = Color.fromRGBO(226, 226, 252, 1.0);
-  static const Color darkLavender = Color.fromRGBO(75, 75, 190, 0.5);
-
-  static const Color lightBlueGray = Color.fromRGBO(239, 241, 243, 1.0);
-
-  // cross theme
-  static Color body = Color.fromRGBO(6, 6, 7, 1.0);
+  static Color selected = const Color.fromRGBO(56, 189, 248, 1.0); // for nav bar icons buttons
+  
+  static Color tooltip = const Color.fromRGBO(71, 85, 105, 0.8); // for pages icon buttons tooltips in top right
 }
+
+  // static Color body = Color.fromRGBO(6, 6, 7, 1.0);
+  // static Color bars = Color.fromRGBO(12, 12, 15, 1.0);
+
+  // static Color text1 = Colors.white;
+  // static Color text2 = Color.fromRGBO(239, 241, 243, 1.0);
+  // static Color text3 = Color.fromRGBO(226, 226, 252, 1.0);
+  // static Color text4 = Colors.white24;
+
+  // static Color selected = Color.fromRGBO(120, 130, 255, 1.0);
+
+  // static Color tooltip = Color.fromRGBO(75, 75, 190, 0.5);
