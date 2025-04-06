ec267793f120f78b30226fad5d14ef6296575f56
Ido-Badash <idoba12012011@gmail.com>
2025-04-06 12:41:38 +0300

    flow: 🔮 Add new background images and update TimerScreen to cycle through them with a button 🏮

--- lib/features/flow/timer.dart
+++ lib/features/flow/timer.dart
@@ -6,7 +6,7 @@ import 'package:flowscape/core/data/flowbg_images.dart';
 // styles
 import 'package:flowscape/core/styles/colors.dart';
 
-final List<String> bgImages = getRandomImagesList(5);
+final List<String> bgImages = getRandomImagesList(12);
 
 class TimerScreen extends StatefulWidget {
   const TimerScreen({super.key});
@@ -16,16 +16,38 @@ class TimerScreen extends StatefulWidget {
 }
 
 class _TimerScreenState extends State<TimerScreen> {
+  int currentImage = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: FlowColors.body,
-      body: SizedBox.expand(
-        child: Image.asset(
-          bgImages[0],
-          fit: BoxFit.cover,
-        ),
+      body: Stack(
+        children: [
+          SizedBox.expand(
+            child: Image.asset(bgImages[currentImage], fit: BoxFit.cover),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: buildNextImageIconButton(),
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget buildNextImageIconButton() {
+    return IconButton(
+      onPressed: () {
+        // Add your button action here
+        setState(() {
+          currentImage = (currentImage + 1) % bgImages.length;
+          debugPrint(currentImage.toString());
+        });
+      },
+      iconSize: 30,
+      icon: Icon(color: FlowColors.text3, Icons.next_plan_outlined),
     );
   }
 }
