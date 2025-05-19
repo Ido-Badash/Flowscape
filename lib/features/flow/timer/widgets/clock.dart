e82dc7dae225bcd6fec74dfd119246da435014d1
Ido-Badash <idoba12012011@gmail.com>
2025-05-19 20:21:08 +0300

    ✨ Refactor Clock widget to StatefulWidget; implement animation controller for dynamic time display.

--- lib/features/flow/timer/widgets/clock.dart
+++ lib/features/flow/timer/widgets/clock.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
 
-class Clock extends StatelessWidget {
+class Clock extends StatefulWidget {
   const Clock({super.key});
 
   @override
+  State<Clock> createState() => _ClockState();
+}
+
+class _ClockState extends State<Clock> with TickerProviderStateMixin {
+  late AnimationController _controller;
+
+  String get countText {
+    Duration count = _controller.duration! * _controller.value;
+    return '';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 60),
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Text(
+      countText,
+      style: const TextStyle(
+        fontSize: 48,
+        fontWeight: FontWeight.bold,
+        color: Colors.white,
+      ),
+    );
   }
 }
