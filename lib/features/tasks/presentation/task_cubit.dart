2e74fc5a18f1d8b9f957a7307c31a8f62a9782e0
Ido-Badash <idoba12012011@gmail.com>
2025-05-24 15:00:08 +0300

    🧾📨✨ Implement TaskCubit for task management; add methods to load, add, delete, and toggle tasks, and update pubspec.yaml and pubspec.lock for flutter_bloc dependency.

---
+++ lib/features/tasks/presentation/task_cubit.dart
@@ -0,0 +1,63 @@
+/*
+
+TASK CUBIT
+simple state managment
+
+each cubit is a list of tasks
+
+*/
+
+import 'package:flowscape/features/tasks/domain/models/task.dart';
+import 'package:flowscape/features/tasks/domain/repo/task_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class TaskCubit extends Cubit<List<Task>> {
+  final TaskRepo taskRepo;
+
+  // constructor: initial state = empty list []
+  TaskCubit(this.taskRepo) : super([]) {
+    loadTasks();
+  }
+
+  // load
+  Future<void> loadTasks() async {
+    // fetch tasks from repo
+    final tasksList = await taskRepo.getAllTasks();
+
+    // emit as new state
+    emit(tasksList);
+  }
+
+  // add
+  Future<void> addTask(String text) async {
+    // create new task with uniqe ID
+    Task newTask = Task(id: DateTime.now().millisecondsSinceEpoch, text: text);
+
+    // save new task to the repo
+    await taskRepo.addTask(newTask);
+
+    // reload the repo
+    loadTasks();
+  }
+
+  // delete
+  Future<void> deleteTask(Task task) async {
+    // delete the task from repo
+    await taskRepo.deleteTask(task);
+
+    // reload the repo
+    loadTasks();
+  }
+
+  // toggle [isComplete] state
+  Future<void> toggleTask(Task task) async {
+    // toggles the [isComplete] state
+    final updatedTask = task.toggleCompleteState();
+
+    // update the task in the repo
+    await taskRepo.updateTask(updatedTask);
+
+    // reload the repo
+    loadTasks();
+  }
+}
