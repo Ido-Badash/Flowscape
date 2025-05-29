2f3044f12cb7afb5e2d6cacea3f7e134eeeea803
Ido-Badash <idoba12012011@gmail.com>
2025-05-29 15:33:56 +0300

    🎡⚱️ Refactor PlaylistModel and SongModel: update properties, enhance methods, and add audio metadata handling

--- lib/features/music/domains/playlist/models/playlist_model.dart
+++ lib/features/music/domains/playlist/models/playlist_model.dart
@@ -6,21 +6,17 @@ This is what a playlist object is.
 --------------------------------------------------------------------------------
 ~~~ PROPERTIES ~~~
 
-*- required:
+* - required:
   - title
       String
 
-  - songs
+  - songModels
       List<SongModel>
 
   - creator
       String
 
-*- optional:
-  - currentSongIdx
-      int
-      ? NOTE: tracks the current idx in the songs list
-
+* - optional:
   - description
       String
 
@@ -31,6 +27,19 @@ This is what a playlist object is.
   - shuffle
       bool
       ? NOTE: give shuffle a value that will be stored in a database [false] by default
+
+  - playlistImagePath
+      String
+
+* - private:
+  - currentSongIdx
+        int
+        ? NOTE: tracks the current idx in the songs list
+        ? NOTE: this is private so it can be changed only by methods
+  
+  - random
+        Random
+        ? NOTE: used for shuffling the songs if shuffle is true
 
 --------------------------------------------------------------------------------
 ~~~ METHODS ~~~
@@ -45,19 +54,6 @@ This is what a playlist object is.
     just like next() but runs backwards
     so instead of idx + 1, its idx - 1
 
-* - play({int atSongIdx = 0})
-    starts playing a song from the start aka idx 0
-    if atSongIdx wasent provided
-
-* - stop()
-    stops a song but keeps track of what song and when was stopped
-    later on the what song and when will be stored in a database
-    so resume() can get to it
-
-* - resume()
-    takes the pending song from the database after stop() was called
-    if resume() is called but stop() wasent its just going to call play()
-
 --------------------------------------------------------------------------------
 ~~~ MODEL NOTES ~~~
 
@@ -66,60 +62,128 @@ This is what a playlist object is.
 */
 
 // Imports
+import 'dart:math';
+
 import 'package:flowscape/features/music/domains/song/model/song_model.dart';
 import 'playlist_order.dart';
 
 // PlaylistModel
 class PlaylistModel {
   // required
+  List<SongModel> songModels;
   final String title;
-  final List<SongModel> songs;
   final String creator;
 
   // optional
-  int _currentSongIdx;
   final String description;
   final PlaylistOrder order;
   final bool shuffle;
+  final String? playlistImagePath;
 
+  // private
+  int _currentSongIdx;
+  final Random _random = Random();
+
+  // getters
   int get currentSongIdx => _currentSongIdx;
+  List<SongModel> get songs => songModels;
+
+  // setters
+  set currentSongIdx(int idx) {
+    if (idx >= 0 && idx < songs.length) {
+      _currentSongIdx = idx;
+    } else {
+      throw RangeError('Index out of range for songs list');
+    }
+  }
+
+  set songs(List<SongModel> songModels) {
+    songs = songModels;
+  }
 
   // constructor
   PlaylistModel({
     int currentSongIdx = 0,
-    required this.songs,
+    int currentSongTime = 0,
+    required this.songModels,
     required this.title,
     required this.creator,
     this.description = "",
     this.order = PlaylistOrder.normal,
     this.shuffle = false,
-  }) : _currentSongIdx = currentSongIdx;
+    this.playlistImagePath,
+  }) : _currentSongIdx = currentSongIdx,
 
-  // TODO: add a [songs] null/empty check
+       assert(songModels.isNotEmpty, 'Songs list cannot be empty');
+
+  void updateSongs() {
+    if (shuffle) {
+      songs.shuffle(_random);
+    }
+    switch (order) {
+      // by creator
+      case PlaylistOrder.byCreator:
+        _orderByCreator();
+      case PlaylistOrder.byCreatorFlipped:
+        _orderByCreator(flipped: true);
+
+      // by time
+      case PlaylistOrder.byTime:
+        _orderByTime();
+      case PlaylistOrder.byTimeFlipped:
+        _orderByTime(flipped: true);
+
+      // by title
+      case PlaylistOrder.byTitle:
+        _orderByTitle();
+      case PlaylistOrder.byTitleFlipped:
+        _orderByTitle(flipped: true);
+
+      // normal and flipped
+      case PlaylistOrder.flipped:
+        _flip();
+      default:
+        songs = songs;
+    }
+  }
+
+  void _orderByCreator({bool flipped = false}) {
+    _willItFlip(
+      flipped,
+      () => songs.sort((a, b) => a.artist.compareTo(b.artist)),
+    );
+  }
+
+  void _orderByTime({bool flipped = false}) {
+    _willItFlip(
+      flipped,
+      () => songs.sort(
+        (a, b) => a.duration.inSeconds.compareTo(b.duration.inSeconds),
+      ),
+    );
+  }
+
+  void _orderByTitle({bool flipped = false}) {
+    _willItFlip(
+      flipped,
+      () => songs.sort((a, b) => a.title.compareTo(b.title)),
+    );
+  }
+
+  void _flip() => songs = songs.reversed.toList();
+
+  void _willItFlip(bool flipped, void Function() func) {
+    func();
+    if (flipped) _flip();
+  }
 
   // NEXT
   void next() {
-    _currentSongIdx = (_currentSongIdx + 1) % songs.length;
+    currentSongIdx = (currentSongIdx + 1) % songs.length;
   }
 
   // PREVIOUS
   void previous() {
-    _currentSongIdx = (_currentSongIdx - 1 + songs.length) % songs.length;
-  }
-
-  // PLAY
-  void play({int atSongIdx = 0}) {
-    _currentSongIdx = atSongIdx % songs.length;
-    // TODO: Add actual play logic here
-  }
-
-  // STOP
-  void stop() {
-    // TODO: Implement stop logic (e.g., store paused state)
-  }
-
-  // RESUME
-  void resume() {
-    // TODO: Resume from stop or call play() if no paused state
+    currentSongIdx = (currentSongIdx - 1 + songs.length) % songs.length;
   }
 }
--- lib/features/music/domains/song/model/song_model.dart
+++ lib/features/music/domains/song/model/song_model.dart
@@ -6,23 +6,135 @@ This is what a song object is.
 --------------------------------------------------------------------------------
 ~~~ PROPERTIES ~~~
 
-*- required:
+* - required:
+  - title
+      String
 
-*- optional:
+  - artist
+      String
+
+  - audioFilePath
+      String
+      ? NOTE: file is an mp3 file and it wil decide how long the song is
+
+* - optional:
+  - album
+      String
+      ? NOTE: the album the song is from
+
+  - coverImagePath
+      String
+      ? NOTE: the path to the cover image of the song
+
+  - genre
+      String
+
+  - lyrics
+      String
+
+  - releaseDate
+      String
+      ? NOTE: the release date of the song in ISO 8601 format
+
+* - private:
+  - _duration
+      int
+      ? NOTE: in seconds, will have the duration of the song file
+
+  - _currentSongTime
+      int
+      ? NOTE: in seconds, will have the current time of the song being played
 
 --------------------------------------------------------------------------------
 ~~~ METHODS ~~~
 
+* - play({int fromTime = 0})
+    plays the song from the start or from a specific time
+
 --------------------------------------------------------------------------------
 ~~~ MODEL NOTES ~~~
-
+TODO: make the class with a async factory constructor
 */
 
 // Imports
+import 'package:audio_metadata_reader/audio_metadata_reader.dart';
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 
 // PlaylistModel
+
 class SongModel {
   // required
-  
+  final String title;
+  final String artist;
+  final String audioFilePath;
+
   // optional
+  final String? album;
+  final String? coverImagePath;
+  final String? genre;
+  final String? lyrics;
+  final String? releaseDate;
+
+  // private
+  Duration _duration; // in seconds, will have the duration of the song file
+  int _currentSongTime; // in seconds
+
+  // getters
+  int get currentSongTime => _currentSongTime;
+  Duration get duration => _duration;
+
+  // setters
+  set currentSongTime(int time) {
+    if (time < 0 || time > duration.inSeconds) {
+      throw ArgumentError(
+        'Current song time must be between 0 and ${duration.inSeconds} seconds.',
+      );
+    }
+    _currentSongTime = time;
+  }
+
+  set duration(Duration duration) {
+    if (duration.inSeconds < 0) {
+      throw ArgumentError('Song duration cannot be negative.');
+    }
+    _duration = duration;
+  }
+
+  // constructor
+  /// Make sure to call `init()` after creating an instance to set the initial duration.
+  SongModel({
+    required this.title,
+    required this.artist,
+    required this.audioFilePath,
+    this.album,
+    this.coverImagePath,
+    this.genre,
+    this.lyrics,
+    this.releaseDate,
+    int currentSongTime = 0,
+  }) : _currentSongTime = currentSongTime,
+       _duration = Duration(seconds: 0); // will be set later when the song is loaded
+
+  Future<void> init() async{
+    await setInitialDuration();
+  }
+
+  Future<void> setInitialDuration() async {
+    File imageFile = File(audioFilePath);
+    if (!await imageFile.exists()) {
+      throw FileSystemException('Audio file does not exist', audioFilePath);
+    }
+    try {
+      final metadata = readMetadata(imageFile, getImage: false);
+      duration = metadata.duration ?? Duration(seconds: 0);
+    } catch (e) {
+      debugPrint('Error reading metadata for $audioFilePath: $e');
+    }
+  }
+
+  // PLAY
+  void play({int fromTime = 0}) {
+    currentSongTime = fromTime;
+  }
 }
